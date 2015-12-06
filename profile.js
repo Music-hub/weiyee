19c1dc1847dbd10b44e5bfb75cd6e5d46b8dea57
avalonsia <johnny654258@gmail.com>
2015-12-07 04:34:31 +0800

    popup redo

--- profile.js
+++ profile.js
@@ -5,17 +5,9 @@ $('.ui.dropdown')
 $(function(){
     $('#signup').click(function(){
 
-    	var windowWidth = $(window).width();
-		var windowHeight = $(window).height();
-		var popupHeight = $('#pop-div1').height();
-		var popupWidth = $('#pop-div1').width();	
-
         $('#bg').css({'display':'block',
     		'position':'fixed'});
         $('#pop-div1').css({'display':'block',
-    		'margin-left' : (windowWidth/2-popupWidth/2)+'px',
-    		'margin-right' : (windowWidth/2-popupWidth/2)+'px',
-			'margin-top' : (-6*windowHeight/11-6*popupHeight/11)+'px',
 			'position':'fixed'});
         $('#pop-div2').css({'display':'none'});
     });
@@ -29,27 +21,11 @@ $(function(){
 $(function(){
     $('#signin').click(function(){
 
-    	var windowWidth = $(window).width();
-		var windowHeight = $(window).height();
-		var popupHeight = $('#pop-div2').height();
-		var popupWidth = $('#pop-div2').width();	
-
         $('#bg').css({'display':'block',
     		'position':'fixed'});
-        if(windowWidth>800){
-        	$('#pop-div2').css({'display':'block',
-    		'margin-left' : (windowWidth/2-popupWidth/2)+'px',
-    		'margin-right' : (windowWidth/2-popupWidth/2)+'px',
-			'margin-top' : (-6*windowHeight/11-6*popupHeight/11)+'px',
-			'position':'fixed'});
-        }
-        else{
-        	$('#pop-div2').css({'display':'block',
-        	'margin-left' : '0px',
-    		'margin-right' : '0px',
-			'margin-top' : (-6*windowHeight/11-6*popupHeight/11)+'px',
+
+        $('#pop-div2').css({'display':'block',
 			'position':'absolute'});
-        }
         
         $('#pop-div1').css({'display':'none'});
     });
