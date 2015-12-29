299c1b62738525fd707c7965ae2acf8fa45a0538
mmis1000 <mmis1000@yahoo.com.tw>
2015-12-29 16:47:07 +0000

    add email login add email register add email register finish

--- profile.js
+++ profile.js
@@ -1,3 +1,27 @@
+// http://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-url-parameter
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+    return query_string;
+}();
+
 $('.ui.dropdown')
   .dropdown()
 ;
@@ -35,17 +59,23 @@ $(function(){
     });
 });
 
+$(function(){
+    console.log(QueryString);
+    if (!QueryString['register-token']) return;
+    
+    $('#bg').css({'display':'block',
+		'position':'fixed'});
+
+      $('#pop-div3').css({'display':'block',
+		'position':'absolute'});
+    $('#bg').off('click');
+});
 $('#fbsignin').click(function(){
 	window.open('/auth/facebook', 'Login...','width=400,height=200,resizeable,scrollbars');
 });
 
 $('#googlesignin').click(function(){
 	window.open('/auth/google', 'Login...','width=400,height=200,resizeable,scrollbars');
-});
-
-$('#email-auth-ajax').submit(function(e){
-    e.preventDefault();
-	$.post('/auth/email/json');
 });
 
 var templete = 
@@ -104,3 +134,159 @@ window.onmessage = function onmessage (ev) {
       break;
   }
 }
+
+
+$('#email-register')
+.form(
+  {
+    email: {
+      identifier  : 'email',
+    rules: [
+        {
+          type   : 'email',
+          prompt : 'Please enter a valid email'
+        }
+      ]
+    }
+  },
+  {
+    onSuccess: function(event, fields){
+      event.preventDefault();
+      console.log('email validate passed');
+      console.log(fields);
+      var APIEndPoint = "/register/email";
+      $.post(APIEndPoint, {
+        email: fields.email
+      }, function (ev) {
+        if (ev.level === "error") {
+          return alert('register failed: fail to send email to ' + fields.email + ' due to ' + ev.message);
+        }
+        alert('successed! please check your mail box and also junk box for register email');
+        $('#bg').css({'display':'none'});
+        $('#pop-div1').css({'display':'none'});
+      })
+      return false;
+    },
+    onFailure: function(formErrors) {
+      // alert(arguments.length);
+      alert(formErrors);
+      return false;
+    }
+  }
+)
+
+
+
+$('#email-register-finish')
+.form(
+  {
+    password: {
+      identifier  : 'password',
+      rules: [
+          {
+            type   : 'minLength[6]',
+            prompt : 'Password must not shorter than 6 words'
+          },
+          {
+            type   : 'empty',
+            prompt : 'Password must not be empty'
+          }
+      ]
+    },/*
+    password2: {
+      identifier  : 'password2',
+      rules: [
+          {
+            type   : 'match[password]',
+            prompt : 'Password must be the same'
+          }
+      ]
+    },*/
+    username: {
+      identifier  : 'username',
+      rules: [
+          {
+            type   : 'empty',
+            prompt : 'username must not be empty'
+          }
+      ]
+    }
+  },
+  {
+    onSuccess: function(event, fields){
+      event.preventDefault();
+      if (fields.password !== fields.password2) {
+        return alert('password must be the same');
+      }
+      console.log(fields);
+      
+      
+      var APIEndPoint = "/register/email/" + QueryString['register-token'];
+      $.post(APIEndPoint, {
+        name: fields.username,
+        password: fields.password
+      }, function (ev) {
+        if (ev.level === "error") {
+          return alert('register failed! Due to: ' + ev.message);
+        }
+        alert('successed! please use your email to log in!');
+      })
+      
+      return false;
+    },
+    onFailure: function(formErrors) {
+      // alert(arguments.length);
+      alert(formErrors);
+      return false;
+    }
+  }
+)
+$('#email-auth-ajax')
+.form(
+  {
+    password: {
+      identifier  : 'password',
+      rules: [
+          {
+            type   : 'empty',
+            prompt : 'Password must not be empty'
+          }
+      ]
+    },
+    username: {
+      identifier  : 'email',
+      rules: [
+          {
+            type   : 'empty',
+            prompt : 'email must not be empty'
+          }
+      ]
+    }
+  },
+  {
+    onSuccess: function(event, fields){
+      event.preventDefault();
+      console.log(fields);
+      
+      
+      var APIEndPoint = "/auth/email/json";
+      $.post(APIEndPoint, {
+        email: fields.email,
+        password: fields.password
+      }, function (ev) {
+        if (ev.level === "error") {
+          return alert('login failed! Due to: ' + ev.message);
+        }
+        alert('successful login!');
+        location.href = '/';
+      })
+      
+      return false;
+    },
+    onFailure: function(formErrors) {
+      // alert(arguments.length);
+      alert(formErrors);
+      return false;
+    }
+  }
+)
